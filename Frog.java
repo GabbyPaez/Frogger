d26b2e1f1d2aa9b4b00df5788065120945e53bf3
6ameron <142824101+6ameron@users.noreply.github.com>
2023-08-25 16:24:25 +0000

    overlaps feature

--- Frog.java
+++ Frog.java
@@ -6,4 +6,10 @@ public class Frog{
 
   private VisibleImage fromImage;
   private static final double FROG_HIEGHT = 48;
+
+  public boolean overlaps(VisibleImage vechicleImage){
+    return true;
+  
+  }
+  public void kill
 }
