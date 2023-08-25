bffd861e0a6ee9f1a0844ad8589d06fe312e4b10
6ameron <142824101+6ameron@users.noreply.github.com>
2023-08-25 16:26:52 +0000

    Fixed the merge branch

--- Frog.java
+++ Frog.java
@@ -7,16 +7,15 @@ public class Frog{
   private VisibleImage fromImage;
   private static final double FROG_HIEGHT = 48;
 
-  public boolean overlaps(VisibleImage vechicleImage){
-    return true;
-  
-  }
-  public void kill
-}
+
 
   public Frog(Image i, DrawingCanvas c) {
     frogImage = new VisibleImage(i, new Location(10, 10), c);
   }
   
+    public boolean overlaps(VisibleImage vechicleImage){
+    return true;
+  
+  }
 }
 
