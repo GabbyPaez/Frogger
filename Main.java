398ded05f3a294413aa76ad0a54200b8d5b55680
GabbyPaez <142824401+GabbyPaez@users.noreply.github.com>
2023-09-27 16:00:57 +0000

    updated speeds

--- Main.java
+++ Main.java
@@ -67,10 +67,10 @@ public class Main extends WindowController implements KeyListener
       // ADD YOUR CODE TO CREATE THE FROG AND THE LANES
 
           gerald = new Frog(getImage("froggy.gif"),getImage("dead_Frog.gif"),getImage("3heart.png"),getImage("2heart.png"),getImage("1heart.png"),getImage("0heart.png"), canvas);
-     mcQueen = new Vehicle(getImage("van_right.gif"), canvas, 20,90, .0035,0,gerald);
-     tax =  new Vehicle(getImage("taxi_right.gif"), canvas, 20,240, .0035,0,gerald);
-     jee = new Vehicle(getImage("jeep_left.gif"), canvas, 550, 170, -.0035,0,gerald);
-     old =  new Vehicle(getImage("oldcar_left.gif"), canvas, 550,320, -.0035,0,gerald);
+     mcQueen = new Vehicle(getImage("van_right.gif"), canvas, 20,90, .0135,0,gerald);
+     tax =  new Vehicle(getImage("taxi_right.gif"), canvas, 20,240, .0195,0,gerald);
+     jee = new Vehicle(getImage("jeep_left.gif"), canvas, 550, 170, -.0235,0,gerald);
+     old =  new Vehicle(getImage("oldcar_left.gif"), canvas, 550,320, -.0115,0,gerald);
      //pad = new Lily(0,10,getImage("lily.jpg"),getImage("lilyFrog.jpg"), canvas);
 
 
